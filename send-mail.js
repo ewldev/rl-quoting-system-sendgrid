6d329859ec05c68bc4e2ed344208426ebf171dd4
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-12-16 15:01:21 -0500

    sgMail added .then .catch

--- send-mail.js
+++ send-mail.js
@@ -4,8 +4,26 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
     to: "edwin.leung2@gmail.com",
     from: "edwin.leung2@gmail.com",
-    subject: "Sendgrid test send",
+    subject: "Sendgrid test send3",
     text: "Sendgrid send mail testing",
-    html: "<strong>Sendgrid send mail testing</strong>",
+    html: "<strong>Sendgrid send mail testing 3</strong>",
 }
-sgMail.send(msg);
+
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+//   sgMail
+//   .send(msg)
+//   .then(() => {}, error => {
+//     console.error(error);
+
+//     if (error.response) {
+//       console.error(error.response.body)
+//     }
+//   });
