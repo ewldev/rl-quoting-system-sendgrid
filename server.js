aa8a49eb526b68db82cafc620bf97247a9dad49e
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-12-22 17:15:04 -0500

    renamed numerous variable names

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/sent", (req, res) => {
 });
 
 app.post("/sendmail", (req, res) => {
-    const { service, category, email } = req.body;
+    const { service, category, compilationTxn, fiftyService, oneFiftyService, oneFiftyPlusService, quote, email } = req.body;
   
     const from = {name:"RL Quoting System", email:"edwin.leung2@gmail.com",};
     const to = "edwin.leung2@gmail.com";
@@ -36,8 +36,13 @@ app.post("/sendmail", (req, res) => {
       <ul>
         <li>Service: ${service}</li>
         <li>Category: ${category}</li>
+        <li>Compilation Transactions ${compilationTxn}</li>
+        <li>50 transactions service ${fiftyService}</li>
+        <li>150 transactions service ${oneFiftyService}</li>
+        <li>150+ transactions service ${oneFiftyPlusService}</li>
+        <li>Quote: ${quote}</li>        
         <li>Email: ${email}</li>
-      </ul>
+      </ul>      
     `;
 //     const output = `
 //     <p>You have a new quotation request</p>
@@ -51,7 +56,10 @@ app.post("/sendmail", (req, res) => {
 //   `;
   
     sendMail(to, from, subject, output);
-    // res.redirect('back');
+    // res.json(output);
+    // res.status(200).json(output);
+      //  res.redirect('/sent');
+  //  res.sendFile(path.join(__dirname, 'views', 'sent.ejs' ));
   });
 
 const PORT = process.env.PORT || 8080;
