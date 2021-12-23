9169089cad618fb572d7cd00debf27477a708ed7
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-12-23 16:29:50 -0500

    moved server app.post sendmail to sendmail.js

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const express = require('express');
 const app = express();
 
 const sendMail = require('./utils/sendMail');
+// const calculate = require('./utils/calculate');
 
-const log = console.log;
+app.use(express.json());
 
 app.use(express.urlencoded({extended: false}));
 app.use('/public', express.static(path.join(__dirname, 'public')))
@@ -15,55 +16,23 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("contact");
+  res.render("contact");
 });
 
 app.get("/sent", (req, res) => {
-    res.render("sent");
-});
+  // console.log('get body.service', res.body.service)
+  res.render("sent");
+})
 
-app.post("/sendmail", (req, res) => {
-    const { service, category, compilationTxn, fiftyService, oneFiftyService, oneFiftyPlusService, quote, email } = req.body;
+app.post("/sendmail", (req, res) => {sendMail (req, res)});
+    
   
-    const from = {name:"RL Quoting System", email:"edwin.leung2@gmail.com",};
-    const to = "edwin.leung2@gmail.com";
-  
-    const subject = "New Quotation Request";
-  
-    const output = `
-      <p>You have a new quotation request</p>
-      <h3>Service Selections</h3>
-      <ul>
-        <li>Service: ${service}</li>
-        <li>Category: ${category}</li>
-        <li>Compilation Transactions ${compilationTxn}</li>
-        <li>50 transactions service ${fiftyService}</li>
-        <li>150 transactions service ${oneFiftyService}</li>
-        <li>150+ transactions service ${oneFiftyPlusService}</li>
-        <li>Quote: ${quote}</li>        
-        <li>Email: ${email}</li>
-      </ul>      
-    `;
-//     const output = `
-//     <p>You have a new quotation request</p>
-//     <h3>Service Selections</h3>
-//     <ul>
-//       <li>Service: ${service}</li>
-//       <li>Category: ${category}</li>
-//       <li>compilation-txn: ${compilation-txn}</li>
-//       <li>Email: ${email}</li>
-//     </ul>
-//   `;
-  
-    sendMail(to, from, subject, output);
-    // res.json(output);
-    // res.status(200).json(output);
-      //  res.redirect('/sent');
-  //  res.sendFile(path.join(__dirname, 'views', 'sent.ejs' ));
-  });
+  //   sendMail(to, from, subject, output);
+    
+  // });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
--- utils/sendMail.js
+++ utils/sendMail.js
@@ -3,12 +3,36 @@ require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendMail = (to, from, subject, text) => {
+const sendMail = (req, res) => {
+    const { service, category, compilationTxn, fiftyService, oneFiftyService, oneFiftyPlusService, quote, email } = req.body;
+    console.log('req.body',req.body)
+
+    
+    const from = {name:"RL Quoting System", email:"edwin.leung2@gmail.com",};
+    const to = "edwin.leung2@gmail.com";
+  
+    const subject = "New Quotation Request";
+  
+    const output = `
+      <p>You have a new quotation request</p>
+      <h3>Service Selections</h3>
+      <ul>
+        <li>Service: ${service}</li>
+        <li>Category: ${category}</li>
+        <li>Compilation Transactions ${compilationTxn}</li>
+        <li>50 transactions service ${fiftyService}</li>
+        <li>150 transactions service ${oneFiftyService}</li>
+        <li>150+ transactions service ${oneFiftyPlusService}</li>
+        <li>Quote: ${quote}</li>        
+        <li>Email: ${email}</li>
+      </ul>      
+    `;
+
     const msg = {
         to,
         from,
         subject,
-        html: text,
+        html: output,
     }
 
     // sgMail.sendMultiple(msg, function (err, result) {
